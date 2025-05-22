b67073229e41d4789b97288bb4f88e174ff85f25
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2025-05-22 11:45:10 +0300

    Added Gunicorn config files

---
+++ gunicorn_config.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+#change to yours
+venv_path = "/usr/local/"
+sys.path.insert(0, os.path.join(venv_path, "lib/python3.11/site-packages"))
+#change to yours
+sys.path.insert(0, "/opt/provision")
+
+bind = "127.0.0.1:8880"
+workers = 3
+timeout = 30
+loglevel = "info"
+wsgi_app = "provision:application"
--- push-nginx-unit-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-
-curl -X PUT --data-binary @nginx-unit-config.json --unix-socket /var/run/control.unit.sock http://localhost/config
