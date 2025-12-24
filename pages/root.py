699865e7b21ccc0b17ddb7667313fd1a75ff32d2
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2025-12-24 10:28:09 +0200

    Total redo of root page gen. code + root page template

--- pages/root.py
+++ pages/root.py
@@ -19,6 +19,7 @@ def index():
             name for name in os.listdir(current_app.config["WEB_FOLDER"])
             if os.path.isdir(os.path.join(current_app.config["WEB_FOLDER"], name))
         ]
+        html_data = []
         for i, s in enumerate(sorted(sites_list, key=natural_key), 1):
             #general check all Nginx sites-available, sites-enabled folder + php pool.d/ are available
             #variable with full path to nginx sites-enabled symlink to the site
@@ -26,123 +27,76 @@ def index():
             ngx_av = os.path.join(current_app.config["NGX_SITES_PATHAV"],s)
             #variable with full path to php pool config of the site
             php_site = os.path.join(current_app.config["PHP_POOL"],s+".conf")
-            #check of nginx and php have active links and configs of the site
-            button_state = "enabled"
+            #If everything is ok, main view:
             if os.path.islink(ngx_site) and os.path.isfile(php_site):
-                #check if redirects are enabled or disabled in nginx site config and set checkbox to the proper state
-                with open(ngx_av, "r", encoding="utf-8") as f:
-                    for line in f:
-                        if line.lstrip().startswith("if ( $request_uri !="):
-                            button_state = "disabled"
-                table += f"""\n<tr>\n<th scope="row" class="table-success">{i}</th>
-                <form method="post" action="/action" id="main_form"></form>
-                <form method="post" action="/redirects_manager" id="redirect_form{s}"></form>
-                <td class="table-success">
-                    <button class="btn btn-danger delete-btn" data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" data-site="{s}" name="delete" form="main_form" onclick="showLoading()" title="Повне та невозвратне видалення сайту та його конфігурації з серверу.">🙅‍♂️Видалити</button>
-                    <button class="btn btn-warning" data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" name="disable" form="main_form" onclick="showLoading()"  title="Тимчасово вимкнути сайт - він не будет оброблятися при запитах зовні,але фізично залишається на сервері.">🚧Вимкнути</button>
-                    <a name="clone" data-bs-toggle="tooltip" data-bs-placement="top" onclick="showLoading()" class="btn btn-success" href="/clone?source_site={s}" style="width: 139px;" title="Взяти за основу даний сайт та зробити копію для іншого домену.">🚻Клонувати</a>
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" id="gitPullButton" name="gitPull" form="main_form" onclick="showLoading()" class="btn btn-primary gitpull-btn" style="margin-top: 5px;" title="Зробити пул із репозиторію для оновлення коду сайту до актуального">♻Оновити код</button>
-                    <a href="/redirects_manager?site={s}" class="btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top" type="submit" name="manager" value="{s}" style="margin-top: 5px; width: 236px;" title="Керування 301-ми редіректами для цього сайту." {button_state}>🚥Редіректи\n(~{count_redirects(s)} шт. вже є)</a><br>
-                    <input type="hidden" name="sitename" value="{s}">
-                    <u>Сайт розгорнут: {getSiteCreated(s)}</u>
-                <td class="table-success">
-                    <input class="selected-site form-check-input chk" type="checkbox" name="selected" value="{s}" form="main_form">
-                    <a href="https://{s}" target="blank">{s}</a>
-                </td>
-                <td class="table-success">
-                <div class="accordion" id="folderAccordion{i}">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne{i}">
-                            <button class="accordion-button collapsed" type="button" style="background-color: #B5FFF1;" data-bs-toggle="collapse" data-bs-target="#collapseOne{i}" aria-expanded="false" aria-controls="collapseOne{i}" data-path="{os.path.join(current_app.config["WEB_FOLDER"],s)}">
-                                + 📁 {os.path.join(current_app.config["WEB_FOLDER"],s)}
-                            </button>
-                        </h2>
-                        <div id="collapseOne{i}" class="accordion-collapse collapse" aria-labelledby="headingOne{i}" data-bs-parent="#folderAccordion{i}">
-                            <div class="accordion-body">
-                                Заватажую...
-                            </div>
-                        </div>
-                    </div>
-                </div></td>
-                <td class="table-success">{getSiteOwner(s)}</td>
-                <td class="table-success">✅OK</td>
-                \n</tr>"""
+                html_data.append({
+                    "table_type": f"<tr>\n<th scope=\"row\" class=\"table-success\">{i}</th>",
+                    "button_2": f"<button class=\"btn btn-warning\" type=\"submit\" value=\"{s}\" name=\"disable\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" form=\"main_form\" onclick=\"showLoading()\" title=\"Тимчасово вимкнути сайт - він не будет оброблятися при запитах зовні,але фізично залишається на сервері.\">🚧Вимкнути</button>",
+                    "site_name": s,
+                    "table_type2": "<td class=\"table-success\">",
+                    "count_redirects": count_redirects(s),
+                    "getSiteCreated": getSiteCreated(s),
+                    "id": i,
+                    "accordeon_path": os.path.join(current_app.config["WEB_FOLDER"],s),
+                    "getSiteOwner": getSiteOwner(s),
+                    "site_status": "✅OK"
+                })
             #if nginx is ok but php is not
             elif os.path.islink(ngx_site) and not os.path.isfile(php_site):
-                table += f"""\n<tr>\n<th scope="row" class="table-danger">{i}</th>
-                <td class="table-danger"><form method="post" action="/action">
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" data-site="{s}" name="delete" onclick="showLoading()" class="btn btn-danger delete-btn" 
-                    title="Повне та невозвратне видалення сайту та його конфігурації з серверу.">🙅‍♂️Видалити</button>
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" name="enable" onclick="showLoading()" class="btn btn-success" 
-                    title="Активувати сайт - він буде оброблятися при запитах ззовні.">Активувати</button>
-                </form>
-                <td class="table-danger">                    
-                    <input class="selected-site form-check-input chk" type="checkbox" name="selected" value="{s}" form="main_form">
-                    <a href="https://{s}" target="blank">{s}</a>
-                </td>
-                <td class="table-danger">{os.path.join(current_app.config["WEB_FOLDER"],s)}</td>
-                <td class="table-danger"></td>
-                <td class="table-danger">🚨Помилка конфігураціх РНР</td>
-                \n</tr>"""
+                html_data.append({
+                    "table_type": f"<tr>\n<th scope=\"row\" class=\"table-danger\">{i}</th>",
+                    "button_2": f"<button class=\"btn btn-success\" type=\"submit\" value=\"{s}\" name=\"enable\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" form=\"main_form\" onclick=\"showLoading()\" title=\"Активувати сайт - він буде оброблятися при запитах ззовні.\">🏃Активувати</button>",
+                    "site_name": s,
+                    "table_type2": "<td class=\"table-danger\">",
+                    "count_redirects": count_redirects(s),
+                    "getSiteCreated": getSiteCreated(s),
+                    "id": i,
+                    "accordeon_path": os.path.join(current_app.config["WEB_FOLDER"],s),
+                    "getSiteOwner": getSiteOwner(s),
+                    "site_status": "🚨Помилка конфігураціх РНР"
+                })
             #if php is ok but nginx is not
             elif not os.path.islink(ngx_site) and os.path.isfile(php_site):
-                table += f"""\n<tr>\n<th scope="row" class="table-danger">{i}</th>
-                <td class="table-danger"><form method="post" action="/action">
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" data-site="{s}" name="delete" onclick="showLoading()" class="btn btn-danger delete-btn" 
-                    title="Повне та невозвратне видалення сайту та його конфігурації з серверу.">🙅‍♂️Видалити</button>
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" name="enable" onclick="showLoading()" class="btn btn-success" 
-                    title="Активувати сайт - він буде оброблятися при запитах ззовні.">🏃Активувати</button>
-                </form>
-                <td class="table-danger">                    
-                    <input class="selected-site form-check-input chk" type="checkbox" name="selected" value="{s}" form="main_form">
-                    <a href="https://{s}" target="blank">{s}</a>
-                </td>
-                <td class="table-danger">{os.path.join(current_app.config["WEB_FOLDER"],s)}</td>
-                <td class="table-danger">{getSiteOwner(s)}</td>
-                <td class="table-danger">🚨Помилка конфігураціх Nginx</td>
-                \n</tr>"""
+                html_data.append({
+                    "table_type": f"<tr>\n<th scope=\"row\" class=\"table-danger\">{i}</th>",
+                    "button_2": f"<button class=\"btn btn-success\" type=\"submit\" value=\"{s}\" name=\"enable\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" form=\"main_form\" onclick=\"showLoading()\" title=\"Активувати сайт - він буде оброблятися при запитах ззовні.\">🏃Активувати</button>",
+                    "site_name": s,
+                    "table_type2": "<td class=\"table-danger\">",
+                    "count_redirects": count_redirects(s),
+                    "getSiteCreated": getSiteCreated(s),
+                    "id": i,
+                    "accordeon_path": os.path.join(current_app.config["WEB_FOLDER"],s),
+                    "getSiteOwner": getSiteOwner(s),
+                    "site_status": "🚨Помилка конфігураціх Nginx"
+                })
             #if really disabled
             elif not os.path.islink(ngx_site) and not os.path.isfile(php_site):
-                table += f"""\n<tr>\n<th scope="row" class="table-warning">{i}</th>
-                <td class="table-warning"><form method="post" action="/action">
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" data-site="{s}" name="delete" onclick="showLoading()" class="btn btn-danger delete-btn" 
-                    title="Повне та невозвратне видалення сайту та його конфігурації з серверу.">🙅‍♂️Видалити</button>
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" type="submit" value="{s}" name="enable" onclick="showLoading()" class="btn btn-success" 
-                    title="Активувати сайт - він буде оброблятися при запитах ззовні.">🏃Активувати</button>
-                    <button data-bs-toggle="tooltip" data-bs-placement="top" style="margin: inherit; margin-top: 1px;" type="submit" value="{s}" name="clone" formaction="/clone" formmethod="post" onclick="showLoading()" class="btn btn-success" 
-                    title="Взяти за основу даний сайт та зробити копію для іншого домену.">🚻Клонувати</button>
-                    Створено: {getSiteCreated(s)}
-                </form>
-                <td class="table-warning">
-                    <input class="selected-site form-check-input chk" type="checkbox" name="selected" value="{s}" form="main_form">
-                    <a href="https://{s}" target="blank">{s}</a>
-                </td>
-                <td class="table-warning">
-                <div class="accordion" id="folderAccordion{i}">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne{i}">
-                            <button class="accordion-button collapsed" type="button" style="background-color: #B5FFF1;" data-bs-toggle="collapse" data-bs-target="#collapseOne{i}" aria-expanded="false" aria-controls="collapseOne{i}" data-path="{os.path.join(current_app.config["WEB_FOLDER"],s)}">
-                                + 📁 {os.path.join(current_app.config["WEB_FOLDER"],s)}
-                            </button>
-                        </h2>
-                        <div id="collapseOne{i}" class="accordion-collapse collapse" aria-labelledby="headingOne{i}" data-bs-parent="#folderAccordion{i}">
-                            <div class="accordion-body">
-                                Заватажую...
-                            </div>
-                        </div>
-                    </div>
-                </div></td>
-                <td class="table-warning">{getSiteOwner(s)}</td>
-                <td class="table-warning">🚧Сайт вимкнено</td>
-                \n</tr>"""
+                html_data.append({
+                    "table_type": f"<tr>\n<th scope=\"row\" class=\"table-warning\">{i}</th>",
+                    "button_2": f"<button class=\"btn btn-success\" type=\"submit\" value=\"{s}\" name=\"enable\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" form=\"main_form\" onclick=\"showLoading()\" title=\"Активувати сайт - він буде оброблятися при запитах ззовні.\">🏃Активувати</button>",
+                    "site_name": s,
+                    "table_type2": "<td class=\"table-warning\">",
+                    "count_redirects": count_redirects(s),
+                    "getSiteCreated": getSiteCreated(s),
+                    "id": i,
+                    "accordeon_path": os.path.join(current_app.config["WEB_FOLDER"],s),
+                    "getSiteOwner": getSiteOwner(s),
+                    "site_status": "🚧Сайт вимкнено"
+                })
             else:
-                table += f"""\n<tr>\n<th scope="row" class="table-danger">{i}</th>
-                <td class="table-danger">ЗАГАЛЬНА</td>
-                <td class="table-danger">ПОМИЛКА</td>
-                <td class="table-danger">СИСТЕМИ</td>
-                <td class="table-danger">Важливі файли або папки не існують</td>
-                \n</tr>"""
-        return render_template("template-main.html",table=table)
+                html_data.append({
+                    "table_type": f"<tr>\n<th scope=\"row\" class=\"table-danger\">{i}</th>",
+                    "button_2": "",
+                    "site_name": "ЗАГАЛЬНА",
+                    "table_type2": "<td class=\"table-danger\">",
+                    "count_redirects": "",
+                    "getSiteCreated": "",
+                    "id": i,
+                    "accordeon_path": "ПОМИЛКА",
+                    "getSiteOwner": "СИСТЕМИ",
+                    "site_status": "Важливі файли або папки не існують"
+                })
+        return render_template("template-main.html",html_data=html_data)
     except Exception as msg:
         logging.error(f"Error in index(/): {msg}")
     return "root.py ERROR!"
