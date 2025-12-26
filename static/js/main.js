0847cd0ad4261b8b73e29a85234bed50a91b1594
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2025-12-26 12:50:20 +0200

    Added Scrool-to-top button

--- static/js/main.js
+++ static/js/main.js
@@ -153,3 +153,19 @@ function saveEditor() {
         }
     });
 }
+
+const btn = document.getElementById("scrollTopBtn");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
