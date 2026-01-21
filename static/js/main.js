6acb8a73c85838259cf3989db435ed80317705a8
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2026-01-21 17:40:37 +0200

    Added quick filter to root page

--- static/js/main.js
+++ static/js/main.js
@@ -172,3 +172,13 @@ btn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+document.getElementById("siteFilter").addEventListener("input", function () {
+  const filter = this.value.toLowerCase();
+  const rows = document.querySelectorAll("tbody tr");
+
+  rows.forEach(row => {
+    const text = row.innerText.toLowerCase();
+    row.style.display = text.includes(filter) ? "" : "none";
+  });
+});
