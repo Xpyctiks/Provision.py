511983f88f857a16380011c66e12333a4a4f3ed2
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2026-01-23 19:58:33 +0200

    Removed Deffered parameter from js loading. Fix for login page js file

---
+++ static/js/login.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const modalElement = document.getElementById("myModal");
+  if (modalElement) {
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+});
+
+function showLoading() {
+  const spinner = document.getElementById("spinnerLoading");
+  if (spinner) {
+    spinner.style.visibility = "visible";
+  }
+}
+
+function hideLoading() {
+  const spinner = document.getElementById("spinnerLoading");
+  if (spinner) {
+    spinner.style.visibility = "hidden";
+  } else {
+    console.warn("Spinner element not found!");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  hideLoading();
+});
+
+window.addEventListener("pageshow", function (event) {
+  if (event.persisted) {
+  hideLoading();
+  }
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
